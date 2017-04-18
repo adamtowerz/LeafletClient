cc50712de8c6cfcb438e56324d865a6a1d470c1c
Zarkoix <adam.towers@me.com>
2017-04-18 09:01:55 -0700

    chore(tests): add tests for LeafletNav and Leaflet/index

---
+++ tests/routes/Leaflet/components/LeafletNav/LeafletNav.spec.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { LeafletNav } from 'routes/Leaflet/components/LeafletNav/LeafletNav.js'
+import { shallow } from 'enzyme'
+
+describe('(Component) LeafletNav', () => {
+  const props = {
+    sections: [
+      { title: '10.3',
+        pages: [
+          { title: '10.3.1 Molecular Dynamics', isFavorited: false, isSelected: false },
+          { title: '10.3.2 High Temperature Interactions', isFavorited: true, isSelected: true },
+          { title: '10.3.3 High Pressure Systems', isFavorited: false, isSelected: false }
+        ]
+      },
+      { title: '10.4',
+        pages: [
+          { title: 'pepe', isFavorited: false, isSelected: false }
+        ]
+      }
+    ]
+  }
+
+  let _component
+  beforeEach(() => {
+    _component = shallow(<LeafletNav sections={props.sections} />)
+  })
+
+  it('Renders the searchbar', () => {
+    const title = _component.find('TextField')
+    expect(title).to.exist
+  })
+
+  it('Renders correct amount of children', () => {
+    expect(_component.find('LeafletSection')).to.have.length(props.sections.length)
+  })
+})
---
+++ tests/routes/Leaflet/index.spec.js
@@ -0,0 +1,17 @@
+import LeafletRoute from 'routes/Leaflet'
+
+describe('(Route) Leaflet', () => {
+  let _route
+
+  beforeEach(() => {
+    _route = LeafletRoute({})
+  })
+
+  it('Should return a route configuration object', () => {
+    expect(typeof _route).to.equal('object')
+  })
+
+  it('Configuration should contain path `counter`', () => {
+    expect(_route.path).to.equal('leaflet')
+  })
+})
